67b57b05bf48c93acd387c9a67804a9f066cfcd8
podvistorcheto <denev.s.deyan@gmail.com>
2020-09-22 08:17:49 +0000

    03-option to create profile added

--- milestone_project4/settings.py
+++ milestone_project4/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'hotel',
     'hotel.booking_functions',
+    'profiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,5 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- milestone_project4/urls.py
+++ milestone_project4/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from profiles import views as profiles_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', profiles_views.register, name='register'),
     path('', include('hotel.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
