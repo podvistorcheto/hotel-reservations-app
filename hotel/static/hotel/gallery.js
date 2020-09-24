d1da957c21e0d6c23b3720b441cd1146a345b553
podvistorcheto <denev.s.deyan@gmail.com>
2020-09-24 14:41:42 +0000

    07-added design of hotel gallery with .js scrolling

---
+++ hotel/static/hotel/gallery.js
@@ -0,0 +1,18 @@
+var current = document.querySelector("#current");
+var imgs = document.querySelectorAll(".imgs img");
+var opacity = 0.4
+
+imgs.forEach(img => 
+    img.addEventListener('click', imgClick));
+
+
+function imgClick(e) {
+    // reset the opacity on picture prevy
+    imgs.forEach(img => (img.style.opacity = 1));
+    // click on the image to make it main photo
+    current.src = e.target.src;
+    // grey-out the grid image which main-photo
+    e.target.style.opacity = opacity;
+}
+
+    
